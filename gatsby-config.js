0c81d3bac39a2ddd03e56f07a4dd7ce6f6a06def
Alejandra Arevalo <44823677+ale22arevalo@users.noreply.github.com>
2024-02-25 22:59:41 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Found in Translation Podcast`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
