b4bc8d8b419ca29adad37e08a7d04515a5abe835
Alejandra Arevalo <44823677+ale22arevalo@users.noreply.github.com>
2024-03-22 21:35:42 -0700

    trying chakra

---
+++ src/components/waveform.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import soundwave4 from "../images/soundwave4.gif"
+
+const Waveform = () => {
+    return (
+        <img className="waveform" src={soundwave4} alt="waveform" />
+    );
+}
+
+export default Waveform
